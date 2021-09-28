7de7e521cdc0b4499f75d3200ed54930c8d9f81e
Rendy Arya Kemal <renrror@gmail.com>
2021-09-28 21:40:03 +0700

    Disable access on editor for non teacher

--- pages/course/[id]/edit.tsx
+++ pages/course/[id]/edit.tsx
@@ -1,12 +1,13 @@
 import { useRouter } from 'next/router'
 import Editor from '../../../components/creator/Editor'
 import Loader from '../../../components/Loader'
-import { useCourse } from '../../../utils/fetchers'
+import { useCourse, useUser } from '../../../utils/fetchers'
 import toast from 'react-hot-toast'
 import { useEffect } from 'react'
 
 const EditCourse = () => {
     const router = useRouter()
+    const { user } = useUser()
     const { id } = router.query
     const { course, error, notFound, isLoading, mutate } = useCourse(id as string)
 
@@ -24,6 +25,15 @@ const EditCourse = () => {
             router.replace('/course')
         }
     }, [course, error])
+
+    useEffect(() => {
+        if (course && user) {
+            if (course.teacher_npm != user.npm) {
+                toast.error('You are not allowed to do so.')
+                router.replace('/course')
+            }
+        }
+    }, [course, user])
 
     if (notFound) {
         router.replace('/course')
