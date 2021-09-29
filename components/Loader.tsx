47e081afdef8b160077bb89885e889ea6f98fc47
Rendy Arya Kemal <renrror@gmail.com>
2021-09-29 10:30:58 +0700

    Change loader to TailSpin

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import LoadingAnimation from 'react-loader-spinner'
 export default function Loader() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <LoadingAnimation type="Circles" color="#2563eb" height={100} width={100} />
+            <LoadingAnimation type="TailSpin" color="#2563eb" height={100} width={100} />
         </div>
     )
 }
